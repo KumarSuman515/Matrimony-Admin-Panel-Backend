8f80dbca775bd8944314720b5786b20077f0e7f8
Suprabha47 <143326844+Suprabha47@users.noreply.github.com>
2025-08-26 13:54:47 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -2,13 +2,13 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.DB,
-  process.env.USER,
-  process.env.PASSWORD || "",
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: process.env.HOST,
+    host: process.env.DB_HOST,
     dialect: "mysql",
-    logging: false,
+    port: process.env.DB_PORT || 3306,
   }
 );
 
